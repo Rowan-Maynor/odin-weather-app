bfa76ed979ace63de82bcd9920a6b468c11aa768
rowan-maynor <rowanmaynor@gmail.com>
2025-01-04 10:32:21 -0500

    Create test function for api call

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//City needs to use underscores instead of spaces, use regex to clean input.
+//Using all lowercase will still return proper capitalized city data.
+async function getWeather() {
+  const response = await fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Atlanta?unitGroup=us&include=days&key=PLZNP3RT4T5J5E6TVMEXRGX6A&contentType=json",
+  );
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+getWeather();
