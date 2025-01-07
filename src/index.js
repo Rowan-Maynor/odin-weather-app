d11368e15a133da438ca82c2bc6be2e697a7773d
rowan-maynor <rowanmaynor@gmail.com>
2025-01-07 17:01:00 -0500

    Allow users to pass city into getWeather

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import "./styles.css";
 if (sessionStorage.getItem("data")){
   console.log(JSON.parse(sessionStorage.getItem("data")));
 } else {
-  getWeather();
+  getWeather("Atlanta");
 }
 
-async function getWeather() {
+async function getWeather(city) {
   const response = await fetch(
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Atlanta?unitGroup=us&include=days&key=PLZNP3RT4T5J5E6TVMEXRGX6A&contentType=json",
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=days&key=PLZNP3RT4T5J5E6TVMEXRGX6A&contentType=json`,
   );
   const weatherData = await response.json();
   sessionStorage.setItem("data", JSON.stringify(weatherData));
