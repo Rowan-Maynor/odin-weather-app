80a8ceebe8c045dd4cd68ccfaebc4906e568f00b
rowan-maynor <rowanmaynor@gmail.com>
2025-01-11 13:05:47 -0500

    Remove sessionStorage
    everything is now handled within getWeather

--- src/index.js
+++ src/index.js
@@ -1,34 +1,19 @@
 import "./styles.css";
 import createWeatherCard from "./weather-card";
 
-//City needs to use underscores instead of spaces, use regex to clean input.
-//Using all lowercase will still return proper capitalized city data.
+const cityName = document.querySelector("#city-name");
 
-//temporarily store data so im not blowing API tokens with every reload
-//Remove when website finished
-let data;
-
-if (sessionStorage.getItem("data")) {
-  console.log(JSON.parse(sessionStorage.getItem("data")));
-  data = JSON.parse(sessionStorage.getItem("data"));
-  generateCards();
-} else {
-  getWeather("Atlanta");
-}
+getWeather("New York");
 
 async function getWeather(city) {
   const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&include=days&key=PLZNP3RT4T5J5E6TVMEXRGX6A&contentType=json`,
   );
   const weatherData = await response.json();
-  sessionStorage.setItem("data", JSON.stringify(weatherData));
-  console.log(JSON.parse(sessionStorage.getItem("data")));
-  data = JSON.parse(sessionStorage.getItem("data"));
-  generateCards();
-}
 
-function generateCards() {
   for (let i = 0; i < 14; i++) {
-    createWeatherCard(data.days[i]);
+    createWeatherCard(weatherData.days[i]);
   }
+
+  cityName.textContent = weatherData.address;
 }
